7bccc49cb9b9b987f24dfda2c020014bdd7065f6
chris <chsantan@microsoft.com>
2017-06-13 00:08:10 +0000

    db conn string

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://chris-cosdb-01:iJBqSSj2dcU90jcDyRaxSHDA0vNWv9LN6F7v9o27ea4oKcZyjadvYLmEdQEGvjlfIBRdYYwPSnGD1tkyDbXOCQ==@chris-cosdb-01.documents.azure.com:10250/?ssl=true',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
