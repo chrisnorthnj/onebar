241851fbc4b8915139f4462cc9d34eaac9bf22b3
Christopher Santaniello <chsantan@microsoft.com>
2017-06-23 09:11:49 -0400

    major breakthrough

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,13 @@
-var app = angular.module('myapp', ['ngTagsInput']);
+var app = angular.module('scotchTodo', ['ngTagsInput']);
 
-app.controller('mainController', function($scope, $http) {
-  $scope.tags = [
-    { name: "Brazil", flag: "Brazil.png" },
-    { name: "Italy", flag: "Italy.png" },
-    { name: "Spain", flag: "Spain.png" },
-    { name: "Germany", flag: "Germany.png" },
-  ];
-  
-  $scope.loadCountries = function($query) {
-    return $http.get('countries.json', { cache: true}).then(function(response) {
-      var countries = response.data;
-      return countries.filter(function(country) {
-        return country.name.toLowerCase().indexOf($query.toLowerCase()) != -1;
-      });
-    });
-  };
+app.controller('tagController', function ($scope, $http) {
+    $scope.tags = [
+        { text: 'Tag1' },
+        { text: 'Tag2' },
+        { text: 'Tag3' }
+    ];
+
+    $scope.loadTags = function (query) {
+        return $http.get('tags.json');
+    };
 });
