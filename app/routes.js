4edadfed42003f65df755fa3f43a7e86f1fe1cd3
Christopher Santaniello <chsantan@microsoft.com>
2017-06-26 11:39:50 -0400

    req.body.tags.text

--- app/routes.js
+++ app/routes.js
@@ -27,7 +27,7 @@ module.exports = function (app) {
         // create a todo, information comes from AJAX request from Angular
         Todo.create({
             text: req.body.text,
-            tags: req.body.tags[],
+            tags: req.body.tags.text,
             done: false
         }, function (err, todo) {
             if (err)
