a70b72a6d5f4e3ff15b47c44bfb952a26a620936
Christopher Santaniello <chsantan@microsoft.com>
2017-06-26 08:38:23 -0400

    count

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,6 @@ var app = angular.module('scotchTodo', ['ngTagsInput']);
 
 app.controller('mainController', function ($scope, $http) {
     $scope.tags = [
-        { text: 'Tag1' },
-        { text: 'Tag2' },
-        { text: 'Tag3' }
     ];
 
     $scope.loadTags = function (query) {
