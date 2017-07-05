4fe0bed65240d210af8d2763af148be65f94ea4d
Christopher Santaniello <chsantan@microsoft.com>
2017-07-05 16:50:53 -0400

    semicolons?

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -56,22 +56,22 @@ angular.module('todoController', [])
         $scope.loadTags = function (query) {
             return $http.get('tags.json');
         }; */
-        $scope.options = ["Text", "Markdown", "HTML", "PHP", "Python", "Java", "JavaScript", "Ruby", "VHDL", "Verilog", "C#", "C/C++"]
-        $scope.tags = ["Markdown", "Ruby"]
+        $scope.options = ["Text", "Markdown", "HTML", "PHP", "Python", "Java", "JavaScript", "Ruby", "VHDL", "Verilog", "C#", "C/C++"];
+        $scope.tags = ["Markdown", "Ruby"];
 
-        $scope.font = null
+        $scope.font = null;
         $scope.fonts = [
             { id: 1, name: "Lucida" },
             { id: 2, name: "DejaVu" },
             { id: 3, name: "Bitstream" },
             { id: 4, name: "Liberation" },
             { id: 5, name: "Verdana" }
-        ]
+        ];
 
-        $scope.font2 = $scope.fonts[1]
+        $scope.font2 = $scope.fonts[1];
 
-        $scope.showName = function (font) { return font.name; }
-        $scope.createName = function (name) { return { name: name } }
+        $scope.showName = function (font) { return font.name; };
+        $scope.createName = function (name) { return { name: name } };
 
         $scope.empty = {
             value: [],
@@ -79,14 +79,14 @@ angular.module('todoController', [])
             addOption: function () {
                 $scope.empty.options.push(Math.random())
             }
-        }
+        };
 
         $scope.selected = function (item) {
             console.log("SELECTED ", item)
-        }
+        };
 
         $scope.foc = function () {
             document.getElementById("s1").focus()
-        }
+        };
     })
 	}]);
